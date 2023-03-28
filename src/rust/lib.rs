5757c7f5ba40e3e192df3e28875cdfd3eb83c1b4
Denny Wong <dwong@forestreet.com>
2023-03-28 09:01:20 +0100

    Corrected a bug with segmentate_all returning the wrong list structure. Refactored segmentate to be Generic in return.

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,7 +1,16 @@
 use pyo3::prelude::*;
 
+#[cfg(feature = "polars")]
+use polars::prelude::*;
+#[cfg(feature = "polars")]
+use pyo3_polars::PySeries;
+// #[cfg(feature="polars")]
+// use pyo3_polars::error::PyPolarsErr;
+
 pub mod config;
 pub mod segmentation;
+
+const DEFAULT_BOUNDS: bool = false;
 
 /// Returns a `List[str]` over substrings of `text` separated on
 /// [UAX#29 sentence boundaries].
@@ -9,7 +18,10 @@ pub mod segmentation;
 /// [UAX#29 sentence boundaries]: http://www.unicode.org/reports/tr29/#Sentence_Boundaries
 #[pyfunction]
 fn segmentate(text: &str, bounds: Option<bool>) -> PyResult<Vec<&str>> {
-    Ok(segmentation::segmentate(text, bounds.unwrap_or(false)))
+    Ok(segmentation::segmentate(
+        text,
+        bounds.unwrap_or(DEFAULT_BOUNDS),
+    ))
 }
 
 /// Returns a `List[List[str]]` over substrings of each `str` in `texts` in sequence,
@@ -24,15 +36,56 @@ fn segmentate_all(
 ) -> PyResult<Vec<Vec<&str>>> {
     Ok(segmentation::segmentate_all(
         &texts,
-        bounds.unwrap_or(false),
+        bounds.unwrap_or(DEFAULT_BOUNDS),
         max_workers,
     ))
 }
+
+/// Returns a `polars.Series[List[str]]` over substrings of each `str` in `texts` in
+/// sequence, separated on [UAX#29 sentence boundaries].
+///
+/// [UAX#29 sentence boundaries]: http://www.unicode.org/reports/tr29/#Sentence_Boundaries
+#[cfg(feature = "polars")]
+#[pyfunction]
+pub fn segmentate_to_polars(text: &str, bounds: Option<bool>) -> PyResult<PySeries> {
+    let series: Series = segmentation::segmentate(text, bounds.unwrap_or(DEFAULT_BOUNDS));
+
+    Ok(PySeries(series))
+}
+
+// #[cfg(feature="polars")]
+// #[pyfunction]
+// pub fn segmentate_all_to_polars(
+//     texts: PySeries,
+//     bounds: Option<bool>,
+//     max_workers: Option<usize>,
+// ) -> PyResult<PyDataFrame> {
+//     let series: Series = texts.into();
+
+//     segmentation::segmentate_multiple_parallel_to_list(
+//         series,
+//         bounds.unwrap_or(DEFAULT_BOUNDS),
+//         max_workers.unwrap_or(4)
+//     )
+//     .map(
+//         |df| PyDataFrame(df)
+//     )
+//     .map_err(
+//         |err| PyPolarsErr::Polars(err).into()
+//     )
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn lib_unicode_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(segmentate, m)?)?;
     m.add_function(wrap_pyfunction!(segmentate_all, m)?)?;
+
+    #[cfg(feature = "polars")]
+    m.add_function(wrap_pyfunction!(segmentate_to_polars, m)?)?;
+
+    // #[cfg(feature="polars")]
+    // m.add_function(wrap_pyfunction!(segmentate_all_to_polars, m)?)?;
+
     Ok(())
 }
--- src/rust/segmentation/mod.rs
+++ src/rust/segmentation/mod.rs
@@ -1,30 +1,45 @@
 use rayon::prelude::*;
 use unicode_segmentation::UnicodeSegmentation;
 
+// #[cfg(feature="polars")]
+// use polars::prelude::*;
+
 use crate::config::CPU_COUNT;
 
-/// Returns a [`Vec<&str>`] over substrings of self separated on
-/// [UAX#29 sentence boundaries].
+/// Returns a generic type over substrings of self separated on
+/// [UAX#29 sentence boundaries]. This is typically a [`Vec<&str>`] or a
+/// [`Series`] (with feature `polars` enabled).
 ///
 /// [UAX#29 sentence boundaries]: http://www.unicode.org/reports/tr29/#Sentence_Boundaries
-pub fn segmentate(text: &str, bounds: bool) -> Vec<&str> {
+pub fn segmentate<'a, T>(text: &'a str, bounds: bool) -> T
+where
+    T: FromIterator<&'a str>,
+{
     if bounds {
-        text.split_sentence_bounds().collect()
+        text.split_sentence_bounds().collect::<T>()
     } else {
-        text.unicode_sentences().collect()
+        text.unicode_sentences().collect::<T>()
     }
 }
 
 /// Internal function running multiple texts through [`segmentate`] in serial.
-fn segmentate_multiple_serial<'a>(texts: &[&'a str], bounds: bool) -> Vec<Vec<&'a str>> {
+///
+/// The generic type `T` is typically a [`Vec<&str>`] or a
+/// [`Series`] (with feature `polars` enabled).
+fn segmentate_multiple_serial<'a, T>(texts: &[&'a str], bounds: bool) -> Vec<T>
+where
+    T: FromIterator<&'a str>,
+{
     texts
         .into_iter()
         .map(|text| segmentate(text, bounds))
         .collect()
 }
 
-/// Internal function running multiple texts through [`segmentate`] in parallel.
-fn segmentate_multiple_parallel<'a>(
+/// Internal function running multiple texts through [`segmentate`] in parallel into a
+/// [`Vec<Vec<&'str>>`]. Using [`Vec`] of [`Vec`] comes with performance penalties, and
+/// requires cloning when moving data back to Python.
+fn segmentate_multiple_parallel_to_vec<'a>(
     texts: &[&'a str],
     bounds: bool,
     max_workers: usize,
@@ -35,16 +50,56 @@ fn segmentate_multiple_parallel<'a>(
         .into_par_iter()
         .fold_chunks(
             chunk_size,
-            || Vec::new(),
+            || Vec::with_capacity(chunk_size),
             |mut v, text| {
-                v.append(&mut segmentate(text, bounds));
+                v.push(segmentate::<Vec<&'a str>>(text, bounds));
                 v
             },
         )
+        .flatten()
         .collect()
 }
 
+// /// Internal function running multiple texts through [`segmentate`] in parallel into a
+// /// [`ChunkedArray`] of [`DataType::List`].
+// #[cfg(feature="polars")]
+// pub fn segmentate_multiple_parallel_to_list<'a>(
+//     texts: Series,
+//     bounds: bool,
+//     max_workers: usize,
+// ) -> Result<DataFrame, PolarsError> {
+//     let chunk_size = (texts.len() as f32 / max_workers as f32).ceil() as usize;
+
+//     let iter: Vec<Series> =
+//         texts
+//         .utf8()?
+//         .par_iter_indexed()
+//         .fold_chunks(
+//             chunk_size,
+//             || Vec::with_capacity(chunk_size),
+//             |mut v, text_opt| {
+//                 if let Some(text) = text_opt {
+//                     // Bad unwrapping, deal with later.
+//                     let mut series = segmentate::<Series>(text, bounds);
+//                     series.rename(text);
+//                     v.push(series);
+//                 } else {}
+//                 v
+//             }
+//         )
+//         .flatten()
+//         .collect()
+//     ;
+
+//     Ok(
+//         DataFrame::from_iter(iter)
+//     )
+// }
+
+/// Returns a `List[List[str]]` over substrings of each `str` in `texts` in sequence,
+/// separated on [UAX#29 sentence boundaries].
 ///
+/// [UAX#29 sentence boundaries]: http://www.unicode.org/reports/tr29/#Sentence_Boundaries
 pub fn segmentate_all<'a>(
     texts: &[&'a str],
     bounds: bool,
@@ -53,7 +108,7 @@ pub fn segmentate_all<'a>(
     let max_workers = max_workers.unwrap_or(CPU_COUNT.clone());
 
     if texts.len() >= max_workers {
-        segmentate_multiple_parallel(texts, bounds, max_workers)
+        segmentate_multiple_parallel_to_vec(texts, bounds, max_workers)
     } else {
         segmentate_multiple_serial(texts, bounds)
     }
